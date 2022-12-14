1203431c81b10e6228073d005c3b94d60d062dc3
Bartosz Augustyniak <bartagst@gmail.com>
2022-12-14 15:23:30 +0100

    added base preprocessing

---
+++ data_augmentation.py
@@ -0,0 +1,15 @@
+from imports import *
+
+import cv2
+import tensorflow as tf
+from keras import layers
+
+resize_and_rescale = tf.keras.Sequential([
+    layers.Resizing(parameters["image_params"]["height"], parameters["image_params"]["width"]),
+    layers.Rescaling(1. / 255),
+])
+
+data_augmentation = tf.keras.Sequential([
+    layers.RandomRotation(0.2),
+
+])
